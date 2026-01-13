20e141205ab69659c889ca2550dbfd93ba01730b
Taminul islam <taminulislam27@gmail.com>
2026-01-13 11:14:50 +0600

    Added A

---
+++ Code/A_Chess_Win.cpp
@@ -0,0 +1,22 @@
+/*
+._____________________..
+|                      |
+|   Md_Taminul_Islam   |
+|   CSE @BU            |
+|______________________|
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define faster() ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(0)
+#define ll long long int
+#define yes cout<<"YES\n"
+#define no cout<<"NO\n"
+#define nl cout<<"\n"
+int main()
+{
+    faster(); 
+    int a, b; cin>>a>>b; 
+    cout<<abs(a-b)+1; 
+    
+    return 0;
+}
